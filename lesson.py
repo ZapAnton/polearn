724aa90ee0db319383e7cff88401f0c2c7c67550
ZapAnton <wc3ft2@gmail.com>
2018-05-02 12:53:35 +0300

    Added mistakes output

--- lesson.py
+++ lesson.py
@@ -1,12 +1,28 @@
 import random
 
 
+class Mistake:
+
+    def __init__(self, question, answer, user_answer):
+        self.question = question
+
+        self.answer = answer
+
+        self.user_answer = user_answer
+
+    def __str__(self):
+        return 'Pytanie: {} Odpowiedź: {} Wasz odpowiedź: {}'\
+            .format(self.question, self.answer, self.user_answer)
+
+
 class Lesson:
 
     def __init__(self, lesson_name: str):
         self.lesson_name = lesson_name
 
         self.lesson_materials = self.__get_lesson_materials()
+
+        self.mistakes = []
 
     def __get_lesson_materials(self) -> dict:
         materials = dict()
@@ -47,9 +63,22 @@ class Lesson:
                 print('\tPomyłka! Prawidłowy odpowiedź: {}'
                       .format(answer_value))
 
+                mistake = Mistake(question_value, answer_value, user_answer)
+
+                self.mistakes.append(mistake)
+
             word_number += 1
 
         self.mistakes_output()
 
     def mistakes_output(self):
-        print('ERRORS:\n')
+        if len(self.mistakes) == 0:
+            print('Wy nie zrobiliście ani jednej pomyłki!\n')
+
+            return
+
+        print('\nPomyłki:')
+        for i, mistake in enumerate(self.mistakes):
+            print('\t{}. {}'.format(i, mistake))
+        else:
+            print('\n')
