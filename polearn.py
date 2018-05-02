6c4e001527de245708f2f4cd44feb1d4290c7218
ZapAnton <wc3ft2@gmail.com>
2018-05-02 03:01:51 +0300

    Added lesson menu

--- polearn.py
+++ polearn.py
@@ -1,2 +1,41 @@
+def start_exercise(exercise_name):
+    print(exercise_name)
+
+
+def choose_lesson_menu():
+    while True:
+        print('1. Liczba mnoja',
+              '2. Stopniowanie przymiotników',
+              '3. Wyjść',
+              sep='\n',
+              end='\n\n')
+
+        print('[Wpisać cyfrę]')
+
+        user_input = None
+
+        try:
+            user_input = int(input('> '))
+        except ValueError:
+            print('Powinniście wpisać cyfre!')
+
+            continue
+
+        if user_input == 1:
+            start_exercise('liczba_mnoga')
+        elif user_input == 2:
+            start_exercise('stopniowanie')
+        elif user_input == 3:
+            print('Do widzenia!')
+
+            return
+        else:
+            print('Mylna cyfra!\n')
+
+            continue
+
+
 if __name__ == '__main__':
-    print('Starting Polish lessongs!')
+    print('Zaczynaliśmy lekcję języka polskego!\n')
+
+    choose_lesson_menu()
